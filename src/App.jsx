cf3a585620c980d1197efe5413ab5db43832643b
touchofstatic <eugenefishberg@gmail.com>
2026-01-18 16:19:58 +0200

    Roulette spinner animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// import { useState, useEffect } from "react";
 import ItemsManager from "./components/ItemsManager.jsx";
 
 export default function App() {
--- src/components/ItemsManager.jsx
+++ src/components/ItemsManager.jsx
@@ -490,17 +490,28 @@ function Item({ item, myListId }) {
 // to be component?
 function Roulette({ items }) {
   const [pull, setPull] = useState("");
+  const [spinner, setSpinner] = useState(false);
 
   function randomize() {
     if (items.length > 0) {
-      let randomIndex = Math.floor(Math.random() * items.length);
-      setPull(items[randomIndex]);
+      setSpinner(true);
+      setTimeout(() => {
+        let randomIndex = Math.floor(Math.random() * items.length);
+        setPull(items[randomIndex]);
+        setSpinner(false);
+      }, 1500);
     }
   }
 
   return (
     <div className="roulette">
       <button onClick={randomize}>[roulette]</button>
+      {spinner && (
+        <span
+          is-="spinner"
+          variant-="dots"
+        ></span>
+      )}
       <div className="roulettePull">{pull.name}</div>
     </div>
   );
