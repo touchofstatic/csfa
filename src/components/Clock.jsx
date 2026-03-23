454a9aa6219ee8f05ce9c8ad6fc4e9ee93270d1e
touchofstatic <eugenefishberg@gmail.com>
2026-03-23 16:19:19 +0200

    Minor fix

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -3,7 +3,8 @@ export default function Clock({ minutes, seconds }) {
     <>
       {/* TODO: kinda lame font? */}
       {/* possibly should go text-7xl on mobile but it seems fine */}
-      <section className="noselect bg-[var(--background1)] text-center text-8xl">
+      {/* Important!!! block padding and leading control the clock no to invisibly overlap the button above and make it unclickable because of text-xl. remove noselect to see */}
+      <section className="noselect bg-[var(--background1)] py-1 text-center text-8xl leading-tight">
         <span>
           {minutes < 10 ? "0" : ""}
           {minutes}
