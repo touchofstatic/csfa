15dc91930aed433ce16f6e9cf5807a74e20ef98e
touchofstatic <eugenefishberg@gmail.com>
2026-03-24 21:40:10 +0200

    Not bad

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import SettingsPomo from "./SettingsPomo";
 
 export default function Navbar() {
   return (
-    <nav className="box-border w-dvw bg-[var(--foreground0)] md:sticky md:top-0">
+    <nav className="box-border w-dvw bg-[var(--foreground0)] shadow-md md:sticky md:top-0">
       <SettingsBoard />
       <SettingsPomo />
       <Themes />
