9c77071323d982511faf2878f95cd490943f4be3
touchofstatic <eugenefishberg@gmail.com>
2026-04-19 18:55:44 +0300

    Find a cooler Clock font

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,10 +1,9 @@
 export default function Clock({ minutes, seconds }) {
   return (
     <>
-      {/* TODO: default font is kinda lame? */}
       {/* Possibly should go text-7xl on mobile but it seems fine */}
       {/* IMPORTANT: block padding and leading control the clock not to invisibly overlap the button above and make it unclickable because of text-xl. Remove noselect to see what happens */}
-      <section className="noselect bg-[var(--background1)] py-1 text-center text-8xl leading-tight">
+      <section className="noselect bg-[var(--background1)] py-1 text-center font-[DotGothic16] text-8xl leading-tight font-bold">
         <span>
           {minutes < 10 ? "0" : ""}
           {minutes}
