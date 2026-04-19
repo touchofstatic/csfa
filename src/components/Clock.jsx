4d5642f1b2b0b3563effdb7c47335617bbac408b
touchofstatic <eugenefishberg@gmail.com>
2026-04-19 19:31:43 +0300

    +1

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -3,7 +3,7 @@ export default function Clock({ minutes, seconds }) {
     <>
       {/* Possibly should go text-7xl on mobile but it seems fine */}
       {/* IMPORTANT: block padding and leading control the clock not to invisibly overlap the button above and make it unclickable because of text-xl. Remove noselect to see what happens */}
-      <section className="noselect bg-[var(--background1)] py-1 text-center font-[DotGothic16] text-8xl leading-tight font-bold">
+      <section className="noselect bg-[var(--background1)] py-1 text-center font-[SUSE_Mono] text-8xl leading-tight font-light">
         <span>
           {minutes < 10 ? "0" : ""}
           {minutes}
