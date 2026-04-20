f8b35aa6338e93c4b781d3eef3bffe76de3f3965
touchofstatic <eugenefishberg@gmail.com>
2026-04-20 18:01:36 +0300

    Flexi IBM font

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -3,7 +3,7 @@ export default function Clock({ minutes, seconds }) {
     <>
       {/* Possibly should go text-7xl on mobile but it seems fine */}
       {/* IMPORTANT: block padding and leading control the clock not to invisibly overlap the button above and make it unclickable because of text-xl. Remove noselect to see what happens */}
-      <section className="noselect bg-[var(--background1)] py-1 text-center font-[SUSE_Mono] text-8xl leading-tight font-light">
+      <section className="noselect bg-[var(--background1)] py-1 text-center font-[flexiIBM] text-9xl leading-tight">
         <span>
           {minutes < 10 ? "0" : ""}
           {minutes}
